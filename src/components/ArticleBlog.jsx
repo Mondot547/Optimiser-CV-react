87c6cfc148cff50ff8607fd0fed8576cff63c75a
Mondot547 <alex.mondot.mail.pro@gmail.com>
2024-06-18 19:23:54 +0200

    modification des titres des articles blog via le css

--- src/components/ArticleBlog.jsx
+++ src/components/ArticleBlog.jsx
@@ -5,8 +5,8 @@ const Article = ({ img, title, description, date }) => {
     return (
         <Card className="my-3">
             <Card.Img variant='top' src={img} />
-            <Card.Body>
-                <Card.Title as="h2" className="fs-3">{title}</Card.Title>
+            <Card.Body className='m-1'>
+                <Card.Title as="h2" className="title-services">{title}</Card.Title>
                 <Card.Text className="fs-6">{description}</Card.Text>
                 <Button variant="primary" rel="noopener noreferrer nofollow" target="_blank">Lire la suite</Button>
             </Card.Body>
